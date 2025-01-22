b602f495ffb7e18ac140808e6e137a20dda14892
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2025-01-22 17:39:49 -0600

    Update prodbees.js

--- kubejs/server_scripts/mods/prodbees.js
+++ kubejs/server_scripts/mods/prodbees.js
@@ -1,13 +1,3 @@
 ServerEvents.recipes(event => {
     if (isExpertMode) {
-    
-        ServerEvents.recipes(event => {
-            const disabledbees = [ 'steel']
-            disabledbees.forEach(bee => {
-
-
-
-
-            })
-        })
     }})
