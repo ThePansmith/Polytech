cbc4d60d4a65fced56e0b6948d25c4b791cb15a1
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2025-02-07 18:56:38 -0600

    Create schema.js

--- kubejs/startup_scripts/schema.js
+++ kubejs/startup_scripts/schema.js
@@ -1,10 +1,13 @@
 /**
  * KubeJS recipe schemas for use in defining custom recipes.
  */
+
+// These have changed in 1.21, so keep that in mind.
 const $RecipeSchema = Java.loadClass('dev.latvian.mods.kubejs.recipe.schema.RecipeSchema');
 const $ShapedRecipeSchema = Java.loadClass('dev.latvian.mods.kubejs.recipe.schema.minecraft.ShapedRecipeSchema');
 const $ShapelessRecipeSchema = Java.loadClass("dev.latvian.mods.kubejs.recipe.schema.minecraft.ShapelessRecipeSchema");
 const $ItemComponents = Java.loadClass('dev.latvian.mods.kubejs.recipe.component.ItemComponents');
+const $FluidComponents = Java.loadClass('dev.latvian.mods.kubejs.recipe.component.FluidComponents');
 const $NumberComponent = Java.loadClass('dev.latvian.mods.kubejs.recipe.component.NumberComponent');
 
 StartupEvents.recipeSchemaRegistry(event => {
@@ -75,9 +78,29 @@ StartupEvents.recipeSchemaRegistry(event => {
     );
     // // Industrial foregoing
     // event.register("industrialforegoing:dissolution_chamber", new $RecipeSchema(
-    //     $ItemComponents.OUTPUT.key("result"),
-    //     $ItemComponents.UNWRAPPED_INPUT_ARRAY.key("input"),
-    //     $NumberComponent.INT.key("processingtime").defaultoptional(200).preferred("processingtime")
+    //     $ItemComponents.OUTPUT.key("output"),
+    //     $FluidComponents.OUTPUT.key("outputFluid").allowEmpty(),
+    //     $ItemComponents.INPUT_ARRAY.key("input"),
+    //     $FluidComponents.INPUT.key("inputFluid"),
+    //     $NumberComponent.INT.key("processingtime").defaultOptional().preferred("processingtime")
     // )
     // );
+    // // Actually Additions
+    // event.register("actuallyadditions:laser", new $RecipeSchema(
+    //     $ItemComponents.OUTPUT.key("result"),
+    //     $ItemComponents.INPUT.key("ingredient"),
+    //     $NumberComponent.INT.key("energy").defaultOptional().preferred("energy")
+    // )
+    // );
+
+    // event.register("actuallyadditions:empowering", new $RecipeSchema(
+    //     $ItemComponents.OUTPUT.key("result"),
+    //     $ItemComponents.INPUT.key("base"),
+    //     $ItemComponents.INPUT_ARRAY.key("modifiers"),
+    //     $NumberComponent.INT.key("energy").defaultOptional().preferred("energy"),
+    //     $NumberComponent.INT.key("color").defaultOptional().preferred("color"),
+    //     $NumberComponent.INT.key("time").defaultOptional().preferred("time")
+    // )
+    // );
+
 });
